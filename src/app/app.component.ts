5e4d1d8a3eb4e74624a3f2aac238ef14813c9b73
vitor-mat <vitormateusalves123@gmail.com>
2022-04-26 00:22:24 -0300

    if some symbol make a sequence on the horizontal the game recognize the player victory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { TmplAstRecursiveVisitor } from '@angular/compiler';
 import { Component } from '@angular/core';
 
+interface IPlayer{
+  title: string;
+  plays: string[];
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,10 +14,25 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   currentlySymbolGame = "x"
+  playerX: IPlayer = {
+    title: "Player X",
+    plays: []
+  }
+  playerO: IPlayer = {
+    title: "Player O",
+    plays: []
+  }
 
-  showSymbolGame(blockElement: HTMLSpanElement){
+  showSymbolGame(blockElement: HTMLSpanElement, blockId: string){
     if(blockElement.textContent === ""){
       blockElement.textContent = this.currentlySymbolGame
+      if(this.currentlySymbolGame === "x"){
+        this.playerX.plays.push(blockId)
+        this.victoryVerification(this.playerX)
+      }else{
+        this.playerO.plays.push(blockId)
+        this.victoryVerification(this.playerO)      
+      }
       this.changeSymbolGame()
     }
   }
@@ -24,6 +44,23 @@ export class AppComponent {
         break;
       default:
         this.currentlySymbolGame = "x"
+    }
+  }
+
+  victoryVerification(player: IPlayer){
+    this.horizontalVerification(player)
+  }
+
+  horizontalVerification(player: IPlayer){
+    if(player.title == "Player X") console.log(this.playerX.plays)
+    if(player.plays.indexOf('block1') !== -1 && player.plays.indexOf('block2') !== -1 && player.plays.indexOf('block3') !== -1){
+      console.log(`${player.title} win`)
+    }
+    if(player.plays.indexOf('block4') !== -1 && player.plays.indexOf('block5') !== -1 && player.plays.indexOf('block6') !== -1){
+      console.log(`${player.title} win`)
+    }
+    if(player.plays.indexOf('block7') !== -1 && player.plays.indexOf('block8') !== -1 && player.plays.indexOf('block9') !== -1){
+      console.log(`${player.title} win`)
     }
   }
 }
